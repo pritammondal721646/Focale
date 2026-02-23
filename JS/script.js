804ce4df43658903c52408b781de250bea5aa68b
pritammondal721646 <pritammondal721646@gmail.com>
2026-02-23 23:40:30 +0530

    first commit

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+// const navbarCollapse = document.querySelector(".navbar-collapse");
+// const menu = document.querySelector(".menu");
+// const body = document.querySelector("body");
+// menu.addEventListener("click", () => {
+//   navbarCollapse.classList.toggle("active");
+//   menu.classList.toggle("animate");
+//   body.style.overflow = "hidden";
+// });
